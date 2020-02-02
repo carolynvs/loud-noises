16a24ec19a2b98f6e3e30a946d3e563784722faa
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-02-02 09:09:01 -0600

    Add slash command structure

--- main.go
+++ main.go
@@ -7,10 +7,31 @@ import (
 	"github.com/nlopes/slack"
 )
 
+type Presence string
+
+const (
+	PresenceAway   = "away"
+	PresenceActive = "auto"
+)
+
+type Action struct {
+	Presence    Presence
+	StatusText  string
+	StatusEmoji string
+	DndMinutes  *int
+}
+
 func main() {
 	token := os.Getenv("SLACK_TOKEN")
 	if token == "" {
 		panic("SLACK_TOKEN not set")
+	}
+
+	action := Action{
+		Presence:    PresenceAway,
+		StatusText:  "Pony Farts!",
+		StatusEmoji: "🦄",
+		DndMinutes:  minutes(60),
 	}
 
 	api := slack.New(token)
@@ -21,19 +42,29 @@ func main() {
 		api.SetDebug(true)
 	}
 
-	err := api.SetUserPresence("away")
+	err := api.SetUserPresence(string(action.Presence))
 	if err != nil {
 		panic(err)
 	}
 
-	err = api.SetUserCustomStatus("brb", ":dash:")
+	err = api.SetUserCustomStatus(action.StatusText, action.StatusEmoji)
 	if err != nil {
 		panic(err)
 	}
 
-	const day = 86400
-	_, err = api.SetSnooze(day)
-	if err != nil {
-		panic(err)
+	if action.DndMinutes == nil {
+		_, err = api.EndSnooze()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, err = api.SetSnooze(*action.DndMinutes)
+		if err != nil {
+			panic(err)
+		}
 	}
+}
+
+func minutes(value int) *int {
+	return &value
 }
---
+++ notes.md
@@ -0,0 +1,51 @@
+# Auth Dance
+https://slack.com/oauth/authorize?client_id=2413351231.504877832356&user_scope=dnd:write,users:write,users.profile:write
+
+## scopes
+* dnd:write - Set yourself to DND and back
+* users:write - Set yourself to away and back
+* users.profile:write - Set your status message / emoji
+
+# Managing Da Noise
+
+## Tell certain workspaces to listen for a named trigger
+
+```
+/create-trigger vacation = vacay (🌴) DND
+
+/create-global-trigger lunch = brb eating lunch (🌯) for 1h
+
+/create-global-trigger sick = out sick (🤒) DND
+
+/create-trigger off-work = DND
+```
+
+## Manual trigger
+
+```
+/trigger vacation for 1w
+/trigger lunch
+/trigger sick for 3d
+```
+
+## Schedule triggers
+
+```
+/schedule-trigger off-work MTuWTh 5pm until tomorrow
+/schedule-trigger off-work F 5pm for 2d
+```
+
+## Show me my triggers
+
+```
+/list-triggers
+```
+
+# Infra
+
+## Frontdoor
+
+https://slackoverload.com
+
+* /* -> container
+* /slack/* -> 
