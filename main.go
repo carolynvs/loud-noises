6ad9ad8d6e48397e34e62f4e1a3f53ab40cc87f7
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-02-07 17:53:10 -0600

    Add /slack/cmd/trigger endpoint

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -11,6 +13,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
 	azureauth "github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/nlopes/slack"
+	"github.com/pkg/errors"
 )
 
 type Presence string
@@ -27,50 +30,108 @@ type Action struct {
 	DndMinutes  *int
 }
 
+/*
+token=gIkuvaNzQIHg97ATvDxqgjtO
+&team_id=T0001
+&team_domain=example
+&enterprise_id=E0001
+&enterprise_name=Globular%20Construct%20Inc
+&channel_id=C2147483705
+&channel_name=test
+&user_id=U2147483697
+&user_name=Steve
+&command=/weather
+&text=94070
+&response_url=https://hooks.slack.com/commands/1234/5678
+&trigger_id=13345224609.738474920.8088930838d88f008e0
+*/
+
 var debugFlag *bool
 
 func main() {
+	debugFlag = flag.Bool("debug", false, "Print debug statements")
+	flag.Parse()
+
+	http.HandleFunc("/slack/cmd/trigger", HandleTrigger)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HandleTrigger(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("text")
+	err := Trigger(name)
+	if err != nil {
+		writer.WriteHeader(500)
+		writer.Write([]byte(err.Error()))
+	}
+}
+
+func Trigger(name string) error {
+	fmt.Printf("/trigger %s\n", name)
+
 	token, err := getSlackToken()
 	if err != nil {
-		panic(err)
+		fmt.Printf("%v\n", err)
+		return err
 	}
 
-	action := Action{
-		Presence:    PresenceAway,
-		StatusText:  "Pony Farts!",
-		StatusEmoji: "🦄",
-		DndMinutes:  minutes(60),
+	actions := map[string]Action{
+		"farts": {
+			Presence:    PresenceAway,
+			StatusText:  "Pony Farts!",
+			StatusEmoji: "🦄",
+			DndMinutes:  minutes(60),
+		},
+		"reset": {
+			Presence:    PresenceActive,
+			StatusText:  "",
+			StatusEmoji: "",
+			DndMinutes:  nil,
+		},
+	}
+
+	action, ok := actions[name]
+	if !ok {
+		err := errors.Errorf("could not trigger %s, action not registered\n", name)
+		fmt.Printf("%v\n", err)
+		return err
 	}
 
 	api := slack.New(token)
-
-	debugFlag = flag.Bool("debug", false, "Print debug statements")
-	flag.Parse()
 	if *debugFlag == true {
 		api.SetDebug(true)
 	}
 
 	err = api.SetUserPresence(string(action.Presence))
 	if err != nil {
-		panic(err)
+		err = errors.Wrap(err, "could not set presence")
+		fmt.Printf("%v\n", err)
+		return err
 	}
 
 	err = api.SetUserCustomStatus(action.StatusText, action.StatusEmoji)
 	if err != nil {
-		panic(err)
+		err = errors.Wrap(err, "could not set status")
+		fmt.Printf("%v\n", err)
+		return err
 	}
 
 	if action.DndMinutes == nil {
 		_, err = api.EndSnooze()
 		if err != nil {
-			panic(err)
+			err = errors.Wrap(err, "could not end do not disturb")
+			fmt.Printf("%v\n", err)
+			return err
 		}
 	} else {
 		_, err = api.SetSnooze(*action.DndMinutes)
 		if err != nil {
-			panic(err)
+			err = errors.Wrap(err, "could not set do not disturb")
+			fmt.Printf("%v\n", err)
+			return err
 		}
 	}
+
+	return nil
 }
 
 func minutes(value int) *int {
